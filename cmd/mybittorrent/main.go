32d55f1811f2706a1b44c22616f6f9a7d48761fa
Ankur Tiwari <ankur@todaypay.me>
2024-12-08 19:28:25 +0530

    issues fixed in list decoding

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -15,74 +15,72 @@ var _ = json.Marshal
 // Example:
 // - 5:hello -> hello
 // - 10:hello12345 -> hello12345
-func decodeBencode(bencodedString string) (interface{}, error) {
-	if unicode.IsDigit(rune(bencodedString[0])) {
+func decodeBencode(bencodedString string, pointer *int) (interface{}, error) {
+	if unicode.IsDigit(rune(bencodedString[*pointer])) {
+		// fmt.Print("in string")
+		// fmt.Print("pointer is ")
+		// fmt.Print(*pointer)
 		var firstColonIndex int
 
-		for i := 0; i < len(bencodedString); i++ {
+		for i := *pointer; i < len(bencodedString)+(*pointer); i++ {
 			if bencodedString[i] == ':' {
 				firstColonIndex = i
+				// fmt.Print("first colon ")
+				// fmt.Print(firstColonIndex)
 				break
 			}
 		}
 
-		lengthStr := bencodedString[:firstColonIndex]
+		lengthStr := bencodedString[*pointer:firstColonIndex]
 
 		length, err := strconv.Atoi(lengthStr)
 		if err != nil {
 			return "", err
 		}
-
+		*pointer = firstColonIndex + 1 + length
 		return bencodedString[firstColonIndex+1 : firstColonIndex+1+length], nil
-	} else if bencodedString[0] == 'i' {
-		numString := bencodedString[1 : len(bencodedString)-1]
-		num, err := strconv.Atoi(numString)
-		if err != nil {
-			return "", fmt.Errorf("error in conversion")
+	} else if bencodedString[*pointer] == 'i' {
+		// fmt.Print("in integer")
+		// fmt.Print("pointer is ")
+		// fmt.Print(*pointer)
+		i := *pointer
+		numStr := ""
+		i++
+		for bencodedString[i]-'0' >= 0 && bencodedString[i]-'0' <= 9 || bencodedString[i] == '-' {
+			numStr += string(bencodedString[i])
+			i++
 		}
+		i--
+		num, err := strconv.Atoi(numStr)
+		if err != nil {
+			return "", fmt.Errorf("error in conversion..")
+		}
+		*pointer = i + 1
 		return num, nil
-	} else if bencodedString[0] == 'l' {
+	} else if bencodedString[*pointer] == 'l' {
 		//it is a list
+		// fmt.Print("in list")
+		// fmt.Print("pointer is ")
+		// fmt.Print(*pointer)
 		list := []interface{}{}
-		// fmt.Print("hi")
-		for i := 1; i < len(bencodedString); i++ {
-			if bencodedString[i] == 'i' {
-				numStr := ""
-				i++
-				for bencodedString[i]-'0' >= 0 && bencodedString[i]-'0' <= 9 || bencodedString[i] == '-' {
-					numStr += string(bencodedString[i])
-					i++
-				}
-				num, err := strconv.Atoi(numStr)
-				if err != nil {
-					return "", fmt.Errorf("error in conversion..")
-				}
-				list = append(list, num)
-				// fmt.Print("appending")
-				// fmt.Print(num)
-			}else if(unicode.IsDigit(rune(bencodedString[i]))){
-				str := ""
-				lenStr := ""
-				for unicode.IsDigit(rune(bencodedString[i])){
-					lenStr += string(bencodedString[i])
-					i++;
-				}
-				i++; //skip in colon
-				length,err:=strconv.Atoi(lenStr)
-				if err!= nil {
-					return "", fmt.Errorf("error in converting for string..")
-				}
-				for k:=0;k<length;k++{
-					str += string(bencodedString[i])
-					i++;
-				}
-				list = append(list, str)
-				// fmt.Print("appending")
-				// fmt.Print(str)
-				i--;
+		*pointer = *pointer + 1
+		for *pointer < len(bencodedString) {
+			result, err := decodeBencode(bencodedString, pointer)
+			// fmt.Print("\n returned with ")
+			// fmt.Print(result)
+			// fmt.Print("\n current index is ")
+			// fmt.Print(*pointer)
+			if err != nil {
+				return "", fmt.Errorf("error in decoding string | err", err)
+			}
+			if result != "" {
+				list = append(list, result)
 			}
 		}
 		return list, nil
+	} else if bencodedString[*pointer] == 'e' {
+		*pointer = *pointer + 1
+		return "", nil
 	} else {
 		return "", fmt.Errorf("Only strings are supported at the moment")
 	}
@@ -98,8 +96,8 @@ func main() {
 		// Uncomment this block to pass the first stage
 		//
 		bencodedValue := os.Args[2]
-
-		decoded, err := decodeBencode(bencodedValue)
+		pointer := 0
+		decoded, err := decodeBencode(bencodedValue, &pointer)
 		if err != nil {
 			fmt.Println(err)
 			return
@@ -112,3 +110,10 @@ func main() {
 		os.Exit(1)
 	}
 }
+
+// lli798e6:bananaee
+
+// 1. [] lli798e6:bananaee
+// 2. [798,] li798e6:bananaee
+
+// 3. returns 798 and 6:bananaee
